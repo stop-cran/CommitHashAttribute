a5ddafed70ba123bf2499679a5303655ad17ff27
stop-cran <stop-cran@list.ru>
2016-11-19 19:42:31 +0100

    Added the project.

---
+++ CommitHash/CommitHashAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CommitHash
+{
+    /// <summary>
+    /// Commit hash to mark source code state.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class CommitHashAttribute : Attribute
+    {
+        /// <param name="value">Commit hash</param>
+        public CommitHashAttribute(string value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Git commit hash
+        /// </summary>
+        public string Value { get; }
+    }
+}
---
+++ CommitHash/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CommitHash
+{
+    public static class Utils
+    {
+        public static string GetCommitHashSingle(string assemblyNameFilter)
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .Where(a => a.GetName().Name.Contains(assemblyNameFilter))
+                .Select(a => a.GetCustomAttribute<CommitHashAttribute>()?.Value)
+                .Distinct()
+                .Single();
+        }
+
+        public static Dictionary<string, string> GetCommitHashes()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .ToDictionary(a => a.FullName, a => a.GetCustomAttribute<CommitHashAttribute>()?.Value);
+        }
+    }
+}
